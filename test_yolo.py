87be320d2f39c65734e4ec06bc8bed3369c48a63
Anytine <otenkigirl@outlook.com>
2025-02-26 17:41:00 +0800

    update

--- test_yolo.py
+++ test_yolo.py
@@ -1,4 +1,4 @@
-from utils.datasets.YOLOv3Dataset import YOLOv3_VOCDataset
-
+from gtorch.utils.datasets.YOLOv3Dataset import YOLOv3_VOCDataset
+from gtorch.utils.misc.plot import plot
 if __name__ == '__main__':
-    dataset = YOLOv3_VOCDataset(download=True)
+    dataset = YOLOv3_VOCDataset(download=False)
