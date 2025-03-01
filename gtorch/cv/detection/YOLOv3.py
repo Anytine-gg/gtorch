d2d7430fcb8fded9a1cee13d9027c8e952e8c01a
Anytine <otenkigirl@outlook.com>
2025-03-02 00:31:02 +0800

    update

--- gtorch/cv/detection/YOLOv3.py
+++ gtorch/cv/detection/YOLOv3.py
@@ -2,80 +2,130 @@ from matplotlib.pyplot import sca
 import torch
 from torch import nn
 import torchsummary
-from gtorch.models.Bottleneck import Bottleneck
+
+
+class DarknetResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        mid_channels = out_channels // 2
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(mid_channels)
+        self.conv2 = nn.Conv2d(
+            mid_channels,
+            out_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False,
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.leaky_relu = nn.LeakyReLU(0.1, inplace=True)
+        # 当下采样（stride>1）或者通道数不匹配时，使用1×1卷积投影
+        self.downsample = None
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(
+                    in_channels, out_channels, kernel_size=1, stride=stride, bias=False
+                ),
+                nn.BatchNorm2d(out_channels),
+            )
+
+    def forward(self, x):
+        out = self.leaky_relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        if self.downsample is not None:
+            x = self.downsample(x)
+        return self.leaky_relu(x + out)
 
 
 class YOLOv3(nn.Module):
     def __init__(self, num_of_classes, in_channels=3):
         super().__init__()
-        out_channels = 3*(5+num_of_classes)
-        self.relu = nn.LeakyReLU(inplace=True)
-        self.conv = nn.Conv2d(in_channels, 32, 3, padding=1, bias=False)
-        self.bn = nn.BatchNorm2d(32)
-        self.res = Bottleneck(32, 64, 2, proj=True)
-        self.seq1 = nn.Sequential(
-            Bottleneck(64, 128, 2, proj=True),
-            Bottleneck(128, 128, 1, proj=False),
-        )
-        self.seq2 = nn.Sequential(
-            Bottleneck(128, 256, 2, proj=True),
-            Bottleneck(256, 256, 1, proj=False),
-            Bottleneck(256, 256, 1, proj=False),
-            Bottleneck(256, 256, 1, proj=False),
-            Bottleneck(256, 256, 1, proj=False),
-            Bottleneck(256, 256, 1, proj=False),
-            Bottleneck(256, 256, 1, proj=False),
-            Bottleneck(256, 256, 1, proj=False)
-        )
-        self.seq3 = nn.Sequential(
-            Bottleneck(256, 512, 2, proj=True),
-            Bottleneck(512, 512, 1, proj=False),
-            Bottleneck(512, 512, 1, proj=False),
-            Bottleneck(512, 512, 1, proj=False),
-            Bottleneck(512, 512, 1, proj=False),
-            Bottleneck(512, 512, 1, proj=False),
-            Bottleneck(512, 512, 1, proj=False),
-            Bottleneck(512, 512, 1, proj=False)
-        )
-        self.seq4 = nn.Sequential(
-            Bottleneck(512, 1024, 2, proj=True),
-            Bottleneck(1024, 1024, 1, proj=False),
-            Bottleneck(1024, 1024, 1, proj=False),
-            Bottleneck(1024, 1024, 1, proj=False)
-        )
-        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')  # 最近邻上采样
+        out_channels = 3 * (5 + num_of_classes)
 
-        self.conv_map3 = nn.Sequential(
-            nn.Conv2d(1024,1024,3,padding=1,bias=False),
-            nn.BatchNorm2d(1024),
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(in_channels, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(inplace=True),
-            nn.Conv2d(1024,out_channels,1)
+            nn.Conv2d(32, 64, 3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
         )
-        self.conv_upsample_from_map3 = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='nearest'),
-            nn.Conv2d(1024,256,1,bias=False),
+        self.layer1 = nn.Sequential(
+            DarknetResidualBlock(64, 64, 1),
+        )
+        self.downsample1 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(inplace=True),
+        )
+        self.layer2 = nn.Sequential(
+            DarknetResidualBlock(128, 128, 1),
+            DarknetResidualBlock(128, 128, 1)
+        )
+        self.downsample2 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, stride=2, padding=1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(inplace=True)
         )
-        self.conv_map2 = nn.Sequential(
-            nn.Conv2d(512+256)
+        self.layer3 = nn.Sequential(
+            DarknetResidualBlock(256, 256, 1),
+            DarknetResidualBlock(256, 256, 1),
+            DarknetResidualBlock(256, 256, 1),
+            DarknetResidualBlock(256, 256, 1),
+            DarknetResidualBlock(256, 256, 1),
+            DarknetResidualBlock(256, 256, 1),
+            DarknetResidualBlock(256, 256, 1),
+            DarknetResidualBlock(256, 256, 1),
         )
+        self.downsample3 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(inplace=True)
+        )
+        self.layer4 = nn.Sequential(
+            DarknetResidualBlock(512, 512, 1),
+            DarknetResidualBlock(512, 512, 1),
+            DarknetResidualBlock(512, 512, 1),
+            DarknetResidualBlock(512, 512, 1),
+            DarknetResidualBlock(512, 512, 1),
+            DarknetResidualBlock(512, 512, 1),
+            DarknetResidualBlock(512, 512, 1),
+            DarknetResidualBlock(512, 512, 1),
+        )
+        self.downsample4 = nn.Sequential(
+            nn.Conv2d(512, 1024, 3, stride=2, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(inplace=True)
+        )
+        self.layer5 = nn.Sequential(
+            DarknetResidualBlock(1024, 1024, 1),
+            DarknetResidualBlock(1024, 1024, 1),
+            DarknetResidualBlock(1024, 1024, 1),
+            DarknetResidualBlock(1024, 1024, 1),
+        )
+        
+        self.upsample = nn.Upsample(scale_factor=2, mode="nearest")  # 最近邻上采样
+
     def forward(self, x):
-        x = self.relu(self.bn(self.conv(x)))
-        x = self.res(x)
-        x = self.seq1(x)
-        x1 = self.seq2(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.downsample1(x)
+        x = self.layer2(x)
+        x = self.downsample2(x)
+        x = self.layer3(x)
+        x = self.downsample3(x)
+        x = self.layer4(x)
+        x = self.downsample4(x)
+        x = self.layer5(x)
         
-        x2 = self.seq3(x1)
-        
-        x3 = self.seq4(x2)
-        x2_up = self.conv_upsample_from_map3(x3)
-        x2_up = torch.cat([x2,x2_up],dim=1)
-        
-        
-        return self.conv_map3(x3)
-if __name__ == '__main__':
-    net = YOLOv3(20,3)
-    net.to('cuda')
-    tensor = torch.rand(3,3,416,416).to('cuda')
-    print(net(tensor).shape)
+        return x
+
+
+if __name__ == "__main__":
+    net = YOLOv3(20, 3)
+    net.to("cuda")
+    tensor = torch.rand(3, 3, 416, 416).to("cuda")
+    #print(net(tensor).shape)
+    torchsummary.summary(net,(3,416,416))
+    
--- gtorch/models/Bottleneck.py
+++ gtorch/models/Bottleneck.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class Bottleneck(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, proj=False,act_fn='leakyrelu'):
+    def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
         mid_channels = out_channels // 4
         self.conv1 = nn.Conv2d(in_channels, mid_channels, 1, bias=False)
@@ -12,25 +12,27 @@ class Bottleneck(nn.Module):
             mid_channels, mid_channels, 3, stride=stride, padding=1, bias=False
         )
         self.bn2 = nn.BatchNorm2d(mid_channels)
-        self.conv3 = nn.Conv2d(mid_channels,out_channels,1,bias=False)
+        self.conv3 = nn.Conv2d(mid_channels, out_channels, 1, bias=False)
         self.bn3 = nn.BatchNorm2d(out_channels)
-        if proj:
-            self.proj_conv = nn.Conv2d(in_channels,out_channels,1,stride=stride,bias=False)
-            self.proj_bn = nn.BatchNorm2d(out_channels)
-        if act_fn == 'leakyrelu':    
-            self.relu = nn.LeakyReLU(inplace=True)
-        else:
-            self.relu = act_fn
-        self.proj = proj
-    def forward(self,x):
+        self.proj = None
+        if stride != 1 or in_channels != out_channels:
+            self.proj = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
         y = self.relu(self.bn1(self.conv1(x)))
         y = self.relu(self.bn2(self.conv2(y)))
         y = self.bn3(self.conv3(y))
         if self.proj:
-            x = self.proj_bn(self.proj_conv(x))
-        return self.relu(x+y)
-if __name__ == '__main__':
-    tensor = torch.rand(3,64,224,224)
-    net = Bottleneck(64,256,stride=2,proj=True)
+            x = self.proj(x)
+        return self.relu(x + y)
+
+
+if __name__ == "__main__":
+    tensor = torch.rand(3, 64, 224, 224)
+    net = Bottleneck(64, 256, stride=2, proj=True)
 
     print(net(tensor).shape)
